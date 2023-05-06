96160d7f81cb907e67ceec9f6f936c131a22c347
Shahmeer Chaudhry <shahmeer_chaudhry@hotmail.com>
2023-05-07 01:44:55 +0930

    Quesiton 7 comments

--- Question7/copy.c
+++ Question7/copy.c
@@ -1,12 +1,17 @@
-//Included for testing
+//Included for testing not needed for function
 #include <stdio.h>
 
 //This assumes source and destination are same size
 //Or Destination has enough space to copy source to
+
+//Using pointers hence do not need to return anything
+
 void copy(char *source, char *destination)
 {
 	int i = 0;
 
+
+	//We run through source and copy each element until null pointer
 	while (source[i] != '\0')
 	{
 
@@ -15,9 +20,12 @@ void copy(char *source, char *destination)
 		i++;
 	}
 
+	//At this point i will be the size of char in source
+	//We add the null pointer to terminate the string
 	destination[i] = '\0';
 
 }
+
 
 int main()
 {
